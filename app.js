bdd39bf753e3ebf25d49daa54280993e65bd164c
Dsoesito <99083937+Dsoesito@users.noreply.github.com>
2022-05-14 17:24:16 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+const sevenBtn = document.getElementById("7-btn");
+const eightBtn = document.getElementById("8-btn");
+const nineBtn = document.getElementById("9-btn");
+const plusBtn = document.getElementById("+btn");
+
+const fourBtn = document.getElementById("4-btn");
+const fiveBtn = document.getElementById("5-btn");
+const sixBtn = document.getElementById("6-btn");
+const minussBtn = document.getElementById("-btn");
+
+const oneBtn = document.getElementById("1-btn");
+const twoBtn = document.getElementById("2-btn");
+const threeBtn = document.getElementById("3-btn");
+const multiplyBtn = document.getElementById("*btn");
+
+const zeroBtn = document.getElementById("0-btn");
+const decimalBtn = document.getElementById(".btn");
+const equalsBtn = document.getElementById("equals-btn");
+const divideBtn = document.getElementById("/btn");
+const clearBtn = document.getElementById("all-clear");
+
+let currentDisplay = document.getElementById("main-screen");
+let miniDisplay = document.getElementById("operation-screen");
+let firstValue = null;
+let secondValue = null;
+
+sevenBtn.addEventListener("click", updateDisplay.bind(null, 7));
+eightBtn.addEventListener("click", updateDisplay.bind(null, 8));
+nineBtn.addEventListener("click", updateDisplay.bind(null, 9));
+plusBtn.addEventListener("click", updateMiniDisplay.bind(null, "+"));
+
+fourBtn.addEventListener("click", updateDisplay.bind(null, 4));
+fiveBtn.addEventListener("click", updateDisplay.bind(null, 5));
+sixBtn.addEventListener("click", updateDisplay.bind(null, 6));
+minussBtn.addEventListener("click", updateMiniDisplay.bind(null, "-"));
+
+oneBtn.addEventListener("click", updateDisplay.bind(null, 1));
+twoBtn.addEventListener("click", updateDisplay.bind(null, 2));
+threeBtn.addEventListener("click", updateDisplay.bind(null, 3));
+multiplyBtn.addEventListener("click", updateMiniDisplay.bind(null, "*"));
+
+zeroBtn.addEventListener("click", updateDisplay.bind(null, 0));
+decimalBtn.addEventListener("click", updateDisplay.bind(null, "."));
+equalsBtn.addEventListener("click", calculate);
+divideBtn.addEventListener("click", updateMiniDisplay.bind(null, "/"));
+clearBtn.addEventListener("click", clear);
+
+function updateDisplay(val) {
+  if (JSON.stringify(currentDisplay.value)[1] == ".") {
+    currentDisplay.value = currentDisplay.value.replace(/^/, "0");
+  }
+  currentDisplay.value = null;
+  currentDisplay.value = currentDisplay.value += val;
+}
+
+function updateMiniDisplay(operation) {
+  if (JSON.stringify(currentDisplay.value)[1] == '"' && operation === "-") {
+    currentDisplay.value = currentDisplay.value.replace(/^/, "-");
+    return;
+  }
+  if (miniDisplay.value !== null) {
+    calculate();
+  }
+  firstValue = JSON.parse(currentDisplay.value);
+  miniDisplay.value = operation;
+}
+
+function clear() {
+  currentDisplay.value = null;
+  miniDisplay.value = null;
+}
+function finalResult(result) {
+  miniDisplay.value = null;
+  currentDisplay.value = result;
+}
+
+function calculate() {
+  if (firstValue === null && secondValue === null) {
+    return;
+  }
+  let result = null;
+  secondValue = JSON.parse(currentDisplay.value);
+  if (miniDisplay.value === "+") {
+    result = firstValue + secondValue;
+    finalResult(result);
+  } else if (miniDisplay.value === "-") {
+    result = firstValue - secondValue;
+    finalResult(result);
+  } else if (miniDisplay.value === "*") {
+    result = firstValue * secondValue;
+    finalResult(result);
+  } else if (miniDisplay.value === "/") {
+    if (firstValue !== 0 && secondValue !== 0) {
+      result = firstValue / secondValue;
+    } else {
+      result = 0;
+    }
+    finalResult(result);
+  } else {
+    return;
+  }
+}
